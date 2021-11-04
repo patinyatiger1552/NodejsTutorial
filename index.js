ee19866af3f7c074138adeac9964f029b40c2afc
tigerdev <patinyatiger1552@gmail.com>
2021-11-04 15:29:58 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs');
+
+const indexPage = fs.readFileSync(`${__dirname}/webpages/index.html`,'utf-8')
+const productPage = fs.readFileSync(`${__dirname}/webpages/product.html`,'utf-8')
 
 const server = http.createServer((req,res)=>{
-  const myHtml = `
-  <h1>Hello BushidoDev</h1>
-  <p style="color:green">Devrogerstack master | 2021</p>
-  `
-  res.write(myHtml)
-  res.end()
+  const pathName = req.url
+  console.log("url = ", pathName)
+  if(pathName === "/" || pathName === "/home"){
+    res.end(indexPage)
+  }else if (pathName === "/product"){
+    res.end(productPage)
+  }else{
+    res.writeHead(404)
+    res.end("<h1>Not Found</h1>")
+  }
 })
 
 server.listen(3000,'localhost',()=>{
